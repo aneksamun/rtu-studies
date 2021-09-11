9efd629a182812821dcfcefba7f593642294e5d8
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2021-09-11 22:07:44 +0100

    Added Database and Project management methodology subjects

--- Bachelor/1st Course/Fall semester/Datoru organizacija un assembleri/Praktiskas/Ievads/Math.asm
+++
@@ -1,9 +0,0 @@
-.model tiny
-.code
-.startup
-org 100h
-mov ax, 5
-add ax,10
-sub ax, 24
-.exit 0
-end
--- Bachelor/1st Course/Fall semester/Datoru organizacija un assembleri/Praktiskas/Ievads/Mul.asm
+++
@@ -1,9 +0,0 @@
-.model tiny
-.code
-.startup
-   org 100h
-   mov al, 5
-   mov bl, -2
-   imul bl
-.exit 0
-end
